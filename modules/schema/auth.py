c9cb97f7d802811100013ffdee8204a6a1ecb12e
LuongThaiSon <luongthaison2k4@gmail.com>
2025-08-20 23:52:39 +0700

    fix auth sign in

--- modules/schema/auth.py
+++ modules/schema/auth.py
@@ -4,14 +4,14 @@ from typing import List , Optional
 from datetime import datetime
 from pydantic import Field
 from pydantic import BaseModel, EmailStr
-
+from typing import Literal
 class SignupSchema(BaseModel):
     username: str
     email: Optional[EmailStr] = None
     full_name: Optional[str] = None
     otp_code: Optional[str] = None
     password: str
-    role: Optional[str] = None
+    role: Literal["user", "admin"] = "user"
     google_id: Optional[str] = None
 
 
